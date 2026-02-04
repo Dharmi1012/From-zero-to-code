e312cda1f20b7f71cdb9040697fa519ca14ab6d5
Dharmi1012 <dharmip1012@gmail.com>
2026-02-04 18:04:34 +0530

    Add files via upload

---
+++ P1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    float a=10,b=15;
+    float c=a+b;
+    float s=a-b;
+    float d=a/b;
+    float m=a*b;
+
+
+            printf("\n add of veriable=%f", c);
+            printf("\n sub of veriable=%f", s);
+            printf("\n div of veriable=%f", d);
+            printf("\n mul of veriable=%f", m);
+
+}
